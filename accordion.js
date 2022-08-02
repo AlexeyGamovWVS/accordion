8304456940411e7d59452e4811da9cfa82e52c04
Alexey Gamov <lexa2009xxxx@yandex.ru>
2022-08-03 00:46:01 +0300

    fix: fix true-force bug

--- accordion.js
+++ accordion.js
@@ -12,7 +12,9 @@ export class Accordion {
   }
 
   _getElements() {
-    this._accordion = document.querySelector(this._config.accordionBlockSelector);
+    this._accordion = document.querySelector(
+      this._config.accordionBlockSelector
+    );
     this._btnSelector = this._config.btnSelector;
     this._itemShowSelector = this._config.itemShowSelector;
     this._itemShowClass = this._itemShowSelector.slice(1);
@@ -24,21 +26,24 @@ export class Accordion {
 
   _setClickListener() {
     this._accordion.addEventListener("click", (e) => {
-      this._targetBtn = e.target.closest(this._btnSelector);
+      this._target = e.target;
+      this._targetBtn = this._target.closest(this._btnSelector);
       this._targetBtn ? this._accordionHandler() : null;
     });
   }
 
   _accordionHandler() {
-    !this._config.alwaysOpen ? this._checkOpenedItem() : null;
+    this._config.alwaysOpen === false ? this._checkOpenedItem() : null;
     this._toggleState(this._targetBtn);
   }
 
   _checkOpenedItem() {
-    const openendItem = this._accordion.querySelector(this._itemShowClass);
+    const openendItem = this._accordion.querySelector(this._itemShowSelector);
     if (openendItem) {
-      this._targetBody = this._targetBtn.parentElement;
-      openendItem !== this._targetBody ? this._toggleState(openendItem) : null;
+      const openendItemBtn = openendItem.querySelector(this._btnSelector);
+      openendItemBtn !== this._target
+        ? this._toggleState(openendItemBtn)
+        : null;
     }
   }
 
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import { Accordion } from "./accordion.js";
 
-const accordion = new Accordion({
-  accordionBlockSelector: ".accordion",
+const firstAcc = new Accordion({
+  accordionBlockSelector: "#acc1",
   btnSelector: ".accordion__btn",
   itemShowSelector: ".accordion__item_show",
-  alwaysOpen: false, //по умолчанию true, можно не добавлять в конфиг, если нужно, чтобы аккордеоны оставались открытыми
+  //alwaysOpen: false, //по умолчанию true, можно не добавлять в конфиг, если нужно, чтобы аккордеоны оставались открытыми
 });
 
-accordion.accordionInit();
+const secondAcc = new Accordion({
+  accordionBlockSelector: "#acc2",
+  btnSelector: ".accordion__btn",
+  itemShowSelector: ".accordion__item_show",
+  alwaysOpen: false, 
+});
+
+firstAcc.accordionInit();
+secondAcc.accordionInit();
